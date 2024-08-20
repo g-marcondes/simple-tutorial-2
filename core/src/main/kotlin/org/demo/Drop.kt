097bc54232a50107fac049fa4fe6a14b5289ea7a
Gabriel Marcondes <gabriel_adorno@hotmail.com>
2024-08-20 10:59:20 +0100

    initial commit

---
+++ core/src/main/kotlin/org/demo/Drop.kt
@@ -0,0 +1,6 @@
+package org.demo
+
+import com.badlogic.gdx.ApplicationAdapter
+
+/** [com.badlogic.gdx.ApplicationListener] implementation shared by all platforms. */
+class Drop : ApplicationAdapter()
---
+++ lwjgl3/src/main/kotlin/org/demo/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,19 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package org.demo.lwjgl3
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import org.demo.Drop
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    // This handles macOS support and helps on Windows.
+    if (StartupHelper.startNewJvmIfRequired())
+      return
+    Lwjgl3Application(Drop(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("libgdx-simple-tutorial-2")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
